ddc39c9b46fee68f5fa20e4c501320b4c5d0c968
Maksim Khrenov <whitetigr2006@rambler.ru>
2012-11-19 09:17:13 +0400

    Added ProteusComModel

---
+++ src/compiler/proteus/uProteusComModel.pas
@@ -0,0 +1,85 @@
+unit uComModel;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, Buttons;
+
+type
+  TfProteusComModel = class(TForm)
+    eByteToSend: TEdit;
+    bSend: TBitBtn;
+    StaticText1: TStaticText;
+    eReceivedByte: TEdit;
+    procedure bSendClick(Sender: TObject);
+    procedure FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  fProteusComModel: TfProteusComModel;
+
+procedure Outport(addr, data: integer);
+function Inport(addr: integer): integer;
+
+implementation
+
+{$R *.dfm}
+
+var
+  comData: integer;
+  comCount: integer = 0;
+  lastComCount: integer = 0;
+
+procedure DoNothing;
+begin
+end;
+
+procedure Outport(addr, data: integer);
+begin
+  case addr of
+    1100: fProteusComModel.eReceivedByte.Text := IntToStr(data and $FF);
+    1101: DoNothing; // we=data; не требуется в модели
+    1102: inc(lastComCount);
+    1104: lastComCount := comCount;
+  else
+    DoNothing;
+  end;
+end;
+
+function Inport(addr: integer): integer;
+begin
+  case addr of
+    1101: Result := comData;
+    1102: Result := integer(comCount <> lastComCount);
+  else
+    Result := not addr;
+  end;
+end;
+
+procedure TfProteusComModel.bSendClick(Sender: TObject);
+var
+  value: integer;
+begin
+  value := StrToIntDef(eByteToSend.Text, -1);
+  if (value < 0) or (value > 255) then
+  begin
+    MessageBox(0, 'Неверное значение байта', 'Ошибка', MB_OK or MB_ICONERROR);
+    Exit;
+  end;
+
+  comData := value;
+  inc(comCount);
+end;
+
+procedure TfProteusComModel.FormCloseQuery(Sender: TObject; var CanClose: Boolean);
+begin
+  CanClose := false;
+  Hide;
+end;
+
+end.
